bcbeb3e5844086f0f586ffdd8b971b217dbe8e6f
Veda Vyas <vedavyas.yeleswarapu@gmail.com>
2017-09-29 21:15:45 +0530

    Oauth2 Error

--- main.py
+++ main.py
@@ -8,6 +8,37 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 import sys
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager, login_required, login_user, \
+    logout_user, current_user, UserMixin
+from requests_oauthlib import OAuth2Session
+from requests.exceptions import HTTPError
+
+root = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+
+class Auth:
+    CLIENT_ID = ('891614416155-5t5babc77fivqfslma1c3u6r2r9fp1o1'
+                 '.apps.googleusercontent.com')
+    CLIENT_SECRET = ' UnGr0t5VT0d3l4PLgICkQoy6 '
+    REDIRECT_URI = 'http://localhost:5000/oauth2callback'
+    AUTH_URI = 'https://accounts.google.com/o/oauth2/auth'
+    TOKEN_URI = 'https://accounts.google.com/o/oauth2/token'
+    USER_INFO = 'https://www.googleapis.com/userinfo/v2/me'
+    SCOPE = ['profile', 'email']
+
+class Config:
+    APP_NAME = "MSIT Course Page"
+    SECRET_KEY = "somethingsecret"
+
+class DevConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(root, "dev.db")
+
+config = {
+    "dev": DevConfig,
+    "default": DevConfig
+}
+
 app = Flask(__name__)
 
 app.debug_log_format = "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s"
@@ -20,16 +51,106 @@ app.logger.setLevel(logging.NOTSET)
 login_log = app.logger
 app.debug = True
 app.secret_key = "some_secret"
+app.config.from_object(config['dev'])
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.session_protection = "strong"
 
+from werkzeug.serving import make_ssl_devcert
+make_ssl_devcert('./ssl', host='localhost')
+
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=True)
+    avatar = db.Column(db.String(200))
+    tokens = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow())
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+def get_google_auth(state=None, token=None):
+    if token:
+        return OAuth2Session(Auth.CLIENT_ID, token=token)
+    if state:
+        return OAuth2Session(
+            Auth.CLIENT_ID,
+            state=state,
+            redirect_uri=Auth.REDIRECT_URI)
+    oauth = OAuth2Session(
+        Auth.CLIENT_ID,
+        redirect_uri=Auth.REDIRECT_URI,
+        scope=Auth.SCOPE)
+    return oauth
 
 @app.route('/')
+@login_required
 def index():
     try:
         return render_template('index.html')
     except Exception as e:
         return render_template('error.html')
 
+@app.route('/login')
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    google = get_google_auth()
+    auth_url, state = google.authorization_url(
+        Auth.AUTH_URI, access_type='offline')
+    session['oauth_state'] = state
+    return auth_url
+
+@app.route('/oauth2callback')
+def callback():
+    if current_user is not None and current_user.is_authenticated:
+        return redirect(url_for('index'))
+    if 'error' in request.args:
+        if request.args.get('error') == 'access_denied':
+            return 'You denied access.'
+        return 'Error encountered.'
+    if 'code' not in request.args and 'state' not in request.args:
+        return redirect(url_for('login'))
+    else:
+        google = get_google_auth(state=session['oauth_state'])
+        try:
+            token = google.fetch_token(
+                Auth.TOKEN_URI,
+                client_secret=Auth.CLIENT_SECRET,
+                authorization_response=request.url)
+        except HTTPError:
+            return 'HTTPError occurred.'
+        google = get_google_auth(token=token)
+        resp = google.get(Auth.USER_INFO)
+        if resp.status_code == 200:
+            user_data = resp.json()
+            email = user_data['email']
+            user = User.query.filter_by(email=email).first()
+            if user is None:
+                user = User()
+                user.email = email
+            user.name = user_data['name']
+            print(token)
+            user.tokens = json.dumps(token)
+            user.avatar = user_data['picture']
+            db.session.add(user)
+            db.session.commit()
+            login_user(user)
+            return redirect(url_for('index'))
+    return 'Could not fetch your information.'
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 @app.route('/module/<number>')
+@login_required
 def module(number=None):
     if number==None:
         return render_template('module1.html')
@@ -39,17 +160,19 @@ def module(number=None):
         except Exception as e:
             return render_template('error.html')
 
-root = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+
 @app.route('/styles/<path:path>')
+@login_required
 def send_stylesheets(path):
     app.logger.info("seeking for %s from %s at %s"%(path, request.headers.get('X-Forwarded-For', request.remote_addr), datetime.now()))
     return send_from_directory(root+"/styles", path)
 
 @app.route('/scripts/<path:path>')
+@login_required
 def send_javascripts(path):
     app.logger.info("seeking for %s from %s at %s"%(path, request.headers.get('X-Forwarded-For', request.remote_addr), datetime.now()))
     return send_from_directory(root+"/scripts", path)
 
 if __name__ == "__main__":
         app.debug = True
-        app.run(host="0.0.0.0")
+        app.run(ssl_context=('./ssl.crt', './ssl.key'))
