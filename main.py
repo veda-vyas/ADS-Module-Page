cf7b8a3ef82546b9b0865c264de4ea6eb8f101a1
Veda Vyas <vedavyas.yeleswarapu@gmail.com>
2017-10-01 17:04:57 +0530

    Added handler to play video

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ from requests.exceptions import HTTPError
 from config import BaseConfig
 import StringIO
 import csv
+import re
+import mimetypes
 
 root = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 
@@ -351,6 +353,44 @@ def downloadStudentActivity():
     if request.method == 'GET':
         data = get_students_activity()
     return render_csv_from_student_activity(data)
+
+
+def send_file_partial(path):
+    range_header = request.headers.get('Range', None)
+    if not range_header: return send_file(path)
+
+    size = os.path.getsize(path)
+    byte1, byte2 = 0, None
+
+    m = re.search('(\d+)-(\d*)', range_header)
+    g = m.groups()
+
+    if g[0]: byte1 = int(g[0])
+    if g[1]: byte2 = int(g[1])
+
+    length = size - byte1
+    if byte2 is not None:
+        length = byte2 + 1 - byte1
+
+    data = None
+    with open(path, 'rb') as f:
+        f.seek(byte1)
+        data = f.read(length)
+
+    rv = ress(data,
+        206,
+        mimetype=mimetypes.guess_type(path)[0],
+        direct_passthrough=True)
+    rv.headers.add('Content-Range', 'bytes {0}-{1}/{2}'.format(byte1, byte1 + length - 1, size))
+    return rv
+
+@app.route('/play/<path:song>')
+def play(song):
+    return send_file_partial(root+"/"+song)
+
+@app.route('/playvideo/<path:song>')
+def playvideo(song):
+    return '<video id="video" controls type="video/mp4" width="100%" src="/play/'+song+'" preload="auto" autoplay>Your browser does not support the video tag.</video>'
 
 if __name__ == "__main__":
         app.debug = True
